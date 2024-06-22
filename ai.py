ae7141e4b6eb8de878956b31fccb8b9e66b20903
Bodebojo <bodebojo@gmail.com>
2024-06-22 11:56:59 +0200

    Added 100% Uncensored model (might need to find one with better prompt template) Switched model path to ./models Resized context window to 4096 tokens

--- ai.py
+++ ai.py
@@ -1,18 +1,20 @@
+from pathlib import Path
 from gpt4all import GPT4All
 
 class Ai:
     def __init__(self, system_prompt: str | None = None, temp: int | None = None):
         # model_id = "nomic-embed-text-v1.5.f16.gguf"  # No text generation; 1GB
         # model_id = "nous-hermes-llama2-13b.Q4_0.gguf"  # Extremely good model. Instruction based. Gives long responses; 16GB # Favorite Censored
-        model_id = "wizardlm-13b-v1.2.Q4_0.gguf"  # Strong overall larger model. Instruction based. Gives very long responses; 16GB # Favorite Uncensored
+        # model_id = "wizardlm-13b-v1.2.Q4_0.gguf"  # Strong overall larger model. Instruction based. Gives very long responses; 16GB # Favorite Uncensored
         # model_id = "orca-mini-3b-gguf2-q4_0.gguf"  # Small version of new model with novel dataset. Very fast responses; 4GB
         # model_id = "gpt4all-falcon-newbpe-q4_0.gguf"  # Very fast model with good quality. Fastest responses; 8GB
         # model_id = "orca-2-13b.Q4_0.gguf"  # Instruction Based. Model for work applications; 16GB
         # model_id = "ghost-7b-v0.9.1-Q4_0.gguf"  # taiwan orso
         # model_id = "gpt4all-13b-snoozy-q4_0.gguf"
-        # model_id = "Phi-3-mini-4k-instruct.Q4_0.gguf"  # Nope
-        # model_id = "Meta-Llama-3-8B-Instruct.Q4_0.gguf"  # Nope
-        self.model = GPT4All(model_id, device='cuda')
+        # model_id = "Lexi-Llama-3-8B-Uncensored-Q6_K.gguf"  # Uncensored but breaks ALOT
+        model_id = "Lexi-Llama-3-8B-Uncensored-Q5_K_S.gguf"  # Uncensored but annoying template
+
+        self.model = GPT4All(model_id, device='cuda', n_ctx=4096, allow_download=False, model_path=Path.cwd() / 'models')
         self.system_prompt = system_prompt
         self.temp = temp
 
